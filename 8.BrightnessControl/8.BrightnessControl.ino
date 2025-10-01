55c834933c3e4cf6091dc874e288902ed3eb2c59
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-10-01 07:17:33 +0530

    Added comment on INO file

--- 8.BrightnessControl/8.BrightnessControl.ino
+++ 8.BrightnessControl/8.BrightnessControl.ino
@@ -1,13 +1,13 @@
 int ledPin = 2;
 
 int ledChannel = 0;
-int frequncy = 5000;
-int resolution = 8;
+int frequncy = 5000; // frequency is how fast the turn on and off should happen
+int resolution = 8;  // how long this should happen 
 
 void setup()
 {
-	ledcSetup(ledChannel, frequncy, resolution);
-	ledcAttachPin(ledPin, ledChannel);
+	ledcSetup(ledChannel, frequncy, resolution); // for making the channel as PWM generator 
+	ledcAttachPin(ledPin, ledChannel);  // for attaching the PIN to channel 
 }
 
 void loop()
