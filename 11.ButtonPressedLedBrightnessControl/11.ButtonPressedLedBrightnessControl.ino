b043fe0c444ebcb8aaea79cb972e4ac0456e0202
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-10-04 20:34:50 +0530

    In this we are making the button control start and stop the brightness control

---
+++ 11.ButtonPressedLedBrightnessControl/11.ButtonPressedLedBrightnessControl.ino
@@ -0,0 +1,38 @@
+int ledPin = 4 ;
+
+int ledChannel  = 0;
+int frequency = 5000;
+int resolution = 8;
+
+int button = 0;
+
+void setup()
+{
+	pinMode(button, INPUT_PULLUP);
+	ledcSetup(ledChannel, frequency, resolution);
+	ledcAttachPin(ledPin, ledChannel);
+	// pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+	if(digitalRead(button) == LOW)
+	{
+		for(int dutyCycle=0;dutyCycle<255;dutyCycle++)
+		{
+			ledcWrite(ledChannel, dutyCycle);
+			delay(10);
+		}
+
+		for(int dutyCycle=255;dutyCycle>0;dutyCycle--)
+		{
+			ledcWrite(ledChannel, dutyCycle);
+			delay(10);
+		}
+		// delay(1000);
+	}
+	else
+	{
+		digitalWrite(ledPin, LOW);
+	}
+}
