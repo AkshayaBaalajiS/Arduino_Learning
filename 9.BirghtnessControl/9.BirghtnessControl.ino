63574b2a6d4495310fd3bff0ab64fc65521817d7
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2025-10-04 20:36:41 +0530

    Brightness control here delay added

--- 9.BirghtnessControl/9.BirghtnessControl.ino
+++ 9.BirghtnessControl/9.BirghtnessControl.ino
@@ -1,5 +1,5 @@
 
-int ledPin = 2;
+int ledPin = 4;
 
 int ledChannel = 0;
 int frequency = 5000;
@@ -16,11 +16,15 @@ void loop()
 	for(int i=0;i<255;i++)
 	{
 		ledcWrite(ledChannel, i);
+		delay(10);
 	}
 
 	for(int i=255;i>0;i--)
 	{
 		ledcWrite(ledChannel, i);
+		delay(10);
+
 	}
+
 	
 }
